62422df7aed36172f8d5bf4b5ca9f5b6c2f80405
TJ2001 <jung.m.tim@gmail.com>
2017-05-23 00:10:56 -0700

    add extends main

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -5,14 +5,21 @@ namespace App\Http\Controllers;
 class PagesController extends Controller {
 
   public function getIndex() {
-    return view('welcome');
+    return view('pages.welcome');
   }
 
   public function getAbout() {
-    return view('about');
+    $first = 'Tim';
+    $last = "Jung";
+    $full = $first . " " . $last;
+    $email = "gentleresolve360@gmail.com";
+    $data = [];
+    $data['fullname'] = $full;
+    $data['email'] = $email;
+    return view('pages.about')->withData($data);
   }
 
   public function getContact(){
-    return view('contact');
+    return view('pages.contact');
   }
 }
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -64,33 +64,9 @@
 
     <div class="container">
 
-      <div class="row">
-        <div class="col-md-12">
-          <h1>Contact Me</h1>
-          <hr>
-          <form>
-            <div class="form-group">
-              <label name="email">Email:</label>
-              <input id="email" name="email" class="form-control">
-            </div>
-
-            <div class="form-group">
-              <label name="subject">Subject:</label>
-              <input id="subject" name="subject" class="form-control">
-            </div>
-
-            <div class="form-group">
-              <label name="message">Message:</label>
-              <textarea id="message" name="message" class="form-control">Type your message here...</textarea>
-            </div>
-
-            <input type="submit" value="Send Message" class="btn btn-success">
-          </form>
-        </div>
-      </div>
+      @yield('content')
 
     </div>
-    <!-- end of .container -->
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
