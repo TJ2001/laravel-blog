0adf51e8e69fc271bd7d9847dab736b591094d53
TJ2001 <jung.m.tim@gmail.com>
2017-05-24 02:42:17 -0700

    add delete and edit buttons for posts

--- resources/views/partials/_messages.blade.php
+++ resources/views/partials/_messages.blade.php
@@ -8,3 +8,14 @@
   {{-- content --}}
 
 @endif
+
+@if (count($errors) > 0)
+  <div class="alert alert-danger">
+    <strong>Errors:</strong>
+    <ul>
+      @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+      @endforeach
+    </ul>
+  </div>
+@endif
